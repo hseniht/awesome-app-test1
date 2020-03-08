d9121298a4e3222ebc8de6cf74baa8567b8dff77
hseniht <thinesh_kumar_mu@hotmail.com>
2020-03-08 13:11:47 +0800

    Updates

---
+++ precache-manifest.46e10cc9b92fc74c5164652eb85eb73b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32507187afae1317dad63d3dd195ee5b",
+    "url": "/awesome-app-test1/index.html"
+  },
+  {
+    "revision": "a828160ea8acb9afd03d",
+    "url": "/awesome-app-test1/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "5e4cec1234c7eba27af9",
+    "url": "/awesome-app-test1/static/css/main.cc9bc9dd.chunk.css"
+  },
+  {
+    "revision": "a828160ea8acb9afd03d",
+    "url": "/awesome-app-test1/static/js/2.011fca3d.chunk.js"
+  },
+  {
+    "revision": "41f2ae5be27a63113cb964beb60661da",
+    "url": "/awesome-app-test1/static/js/2.011fca3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e4cec1234c7eba27af9",
+    "url": "/awesome-app-test1/static/js/main.82bbace0.chunk.js"
+  },
+  {
+    "revision": "24c02e3925c2706c0521",
+    "url": "/awesome-app-test1/static/js/runtime-main.de2b6776.js"
+  }
+]);
