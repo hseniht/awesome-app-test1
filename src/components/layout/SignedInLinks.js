b6453272e73a0a6b455bed6e21fd2ec0bf157734
hseniht <thinesh_kumar_mu@hotmail.com>
2020-03-08 13:42:57 +0800

    5th - color change

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom';
 const SignedInLinks = () => {
     return (
         <ul className="right">
-            <li><NavLink to='/create'>New Project</NavLink></li>
+            <li><NavLink to='/create'>New Post</NavLink></li>
             <li><NavLink to='/'>Log Out</NavLink></li>
             <li>
-            <NavLink to='/' className="btn btn-floating pink lighten-1">TK</NavLink>
+            <NavLink to='/' className="btn btn-floating pink lighten-1">T.K</NavLink>
             </li>
         </ul>
     )
